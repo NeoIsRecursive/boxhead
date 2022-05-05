fb981e5f99d7ef5826092de040b9d886cec35381
Neo Lejondahl <neo.leijondahl@gmail.com>
2022-05-05 18:38:25 +0200

    Change keys array to map and make it a property on window for global access

--- src/scripts/player/Controller.ts
+++ src/scripts/player/Controller.ts
@@ -1,8 +1,18 @@
-const keys: Boolean[] = [];
+declare global {
+  interface Window {
+    keys: Map<string, boolean>;
+  }
+}
 
-window.addEventListener('keydown', function (e) {
-  keys[e.key] = true;
-});
-window.addEventListener('keyup', function (e) {
-  keys[e.key] = false;
-});
+const setUpKeys = () => {
+  window.keys = new Map<string, boolean>();
+
+  window.addEventListener('keydown', function (e) {
+    window.keys.set(e.key, true);
+  });
+  window.addEventListener('keyup', function (e) {
+    window.keys.set(e.key, false);
+  });
+};
+
+export default setUpKeys;
--- src/scripts/player/Player.ts
+++ src/scripts/player/Player.ts
@@ -3,22 +3,35 @@ import * as PIXI from 'pixi.js';
 class Player {
   id: number;
   sprite: PIXI.Sprite;
-  position = { x: 40, y: 50 };
+  position = { x: 0, y: 50 };
   controlls = { up: 'w', left: 'a', down: 's', right: 'd' };
+  size = { height: 64, width: 32 };
+  goingUp = (): boolean => window.keys.get(this.controlls.up) || false;
+  goingDown = (): boolean => window.keys.get(this.controlls.down) || false;
+  goingLeft = (): boolean => window.keys.get(this.controlls.left) || false;
+  goingRight = (): boolean => window.keys.get(this.controlls.right) || false;
   speed = 4;
 
   constructor(id: number, app: PIXI.Application) {
     this.id = id;
+
     this.sprite = PIXI.Sprite.from(character);
+
+    this.sprite.height = this.size.height;
+    this.sprite.width = this.size.width;
+
+    this.position.x = app.screen.width / 2;
+    this.position.y = app.screen.height / 2;
+
     app.stage.addChild(this.sprite);
   }
 
-  update(dt: number, keys: boolean[]) {
+  update(dt: number) {
     const speed = this.speed * dt;
-    if (keys[this.controlls.up]) this.position.y -= speed;
-    if (keys[this.controlls.down]) this.position.y += speed;
-    if (keys[this.controlls.left]) this.position.x -= speed;
-    if (keys[this.controlls.right]) this.position.x += speed;
+    if (this.goingUp()) this.position.y -= speed;
+    if (this.goingDown()) this.position.y += speed;
+    if (this.goingLeft()) this.position.x -= speed;
+    if (this.goingRight()) this.position.x += speed;
   }
 
   draw() {
