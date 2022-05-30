338b14d17a902974ef624adb1ac05ac43026ece0
DavisDavisDavis <olizoooom@gmail.com>
2022-05-30 18:48:22 +0200

    Bullets get fired in player direction
    still need direction to work for when the player is standing still and i have no idea if this is preformant

--- src/scripts/weapons/Bullet.ts
+++ src/scripts/weapons/Bullet.ts
@@ -1,37 +1,24 @@
 import * as PIXI from 'pixi.js';
+import Player from '../player/Player';
 
 export default class Bullet {
   constructor(App: PIXI.Application) {
     this.app = App;
     this.speed = 5;
-    this.direction = { left: 0, right: 0, top: 0, down: 0 };
+    this.direction = { left: false, right: false, up: false, down: false };
   }
-  bullet;
+  hitBox;
   app;
   speed;
   direction;
 
-  shootBullet() {
-    this.bullet = new PIXI.Graphics();
-    this.bullet.beginFill(0xff0000);
-    this.bullet.x = 0;
-    this.bullet.drawRect(this.player.position.x, this.player.position.y, 5, 5);
-    this.bullet.endFill();
-    this.app.stage.addChild(this.bullet);
-
-    switch (this.direction) {
-      case -1:
-        this.bullet.x += this.speed;
-        break;
-      case 1:
-        this.bullet.x += this.speed;
-        break;
-      case 1:
-        this.bullet.x += this.speed;
-        break;
-      case -1:
-        this.bullet.x += this.speed;
-        break;
-    }
+  //I hate having to take in player into this function...
+  shootBullet(Player: Player) {
+    this.hitBox = new PIXI.Graphics();
+    this.hitBox.beginFill(0xff0000);
+    this.hitBox.x = 0;
+    this.hitBox.drawRect(Player.position.x, Player.position.y, 5, 5);
+    this.hitBox.endFill();
+    this.app.stage.addChild(this.hitBox);
   }
 }
--- src/scripts/weapons/Weapon.ts
+++ src/scripts/weapons/Weapon.ts
@@ -2,6 +2,7 @@ import * as PIXI from 'pixi.js';
 import { Vector } from 'p5js-vector-standalone';
 import Player from '../player/Player';
 import { Graphics } from 'pixi.js';
+import Bullet from './Bullet';
 
 export default class Weapon {
   constructor(App: PIXI.Application, Player: Player) {
@@ -11,8 +12,8 @@ export default class Weapon {
     this.speed = 5;
   }
   player;
-  bullet: PIXI.Graphics;
-  bullets = [PIXI.Graphics];
+  bullet: Bullet;
+  bullets = [];
   speed;
   hasFired;
   app;
@@ -21,30 +22,22 @@ export default class Weapon {
     if (this.player.firing()) {
       this.hasFired = true;
       console.log('boom');
+      this.bullet = new Bullet(this.app);
+      this.bullet.shootBullet(this.player);
 
-      this.bullet = new PIXI.Graphics();
-      this.bullet.beginFill(0xff0000);
-      this.bullet.x = 0;
-      this.bullet.drawRect(
-        this.player.position.x,
-        this.player.position.y,
-        5,
-        5
-      );
-      this.bullet.endFill();
-      this.app.stage.addChild(this.bullet);
+      if (this.player.goingLeft()) this.bullet.direction.left = true;
+      if (this.player.goingRight()) this.bullet.direction.right = true;
+      if (this.player.goingUp()) this.bullet.direction.up = true;
+      if (this.player.goingDown()) this.bullet.direction.down = true;
+
       this.bullets.push(this.bullet);
-
-      if (this.player.goingUp()) console.log('fire up');
-      if (this.player.goingDown()) console.log('fire down');
-      if (this.player.goingLeft()) console.log('fire left');
-      if (this.player.goingRight()) console.log('fire right');
     }
     if (this.hasFired) {
-      console.log(this.bullets);
-
-      this.bullets.forEach((bullet) => {
-        bullet.x += dt * this.speed;
+      this.bullets.forEach((bullet: Bullet) => {
+        if (bullet.direction.left) bullet.hitBox.x -= bullet.speed;
+        if (bullet.direction.right) bullet.hitBox.x += bullet.speed;
+        if (bullet.direction.up) bullet.hitBox.y -= bullet.speed;
+        if (bullet.direction.down) bullet.hitBox.y += bullet.speed;
       });
     }
   }
