aba2d523f6aed7580c897bc390b5730696b9f900
DavisDavisDavis <olizoooom@gmail.com>
2022-05-30 14:22:13 +0200

    Several bullets can be fired 🔫

--- src/scripts/weapons/Weapon.ts
+++ src/scripts/weapons/Weapon.ts
@@ -6,38 +6,46 @@ import { Graphics } from 'pixi.js';
 export default class Weapon {
   constructor(App: PIXI.Application, Player: Player) {
     this.player = Player;
-    this.hitBox = new PIXI.Graphics();
     this.app = App;
+
+    this.speed = 5;
   }
   player;
-  hitBox;
+  bullet: PIXI.Graphics;
+  bullets = [PIXI.Graphics];
+  speed;
   hasFired;
   app;
 
   fire(dt) {
     if (this.player.firing()) {
       this.hasFired = true;
-
       console.log('boom');
 
-      this.hitBox.lineStyle({ width: 4, color: 0xff3300, alpha: 1 });
-      this.hitBox.beginFill(0x66ccff);
-      this.hitBox.drawRect(
+      this.bullet = new PIXI.Graphics();
+      this.bullet.beginFill(0xff0000);
+      this.bullet.x = 0;
+      this.bullet.drawRect(
         this.player.position.x,
         this.player.position.y,
         5,
         5
       );
-      this.hitBox.endFill();
+      this.bullet.endFill();
+      this.app.stage.addChild(this.bullet);
+      this.bullets.push(this.bullet);
 
-      this.app.stage.addChild(this.hitBox);
       if (this.player.goingUp()) console.log('fire up');
       if (this.player.goingDown()) console.log('fire down');
       if (this.player.goingLeft()) console.log('fire left');
       if (this.player.goingRight()) console.log('fire right');
     }
     if (this.hasFired) {
-      this.hitBox.x += dt * 1;
+      console.log(this.bullets);
+
+      this.bullets.forEach((bullet) => {
+        bullet.x += dt * this.speed;
+      });
     }
   }
 
