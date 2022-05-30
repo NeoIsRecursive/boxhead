fa9742fca73aab50595048f279dff98d977d3e52
DavisDavisDavis <olizoooom@gmail.com>
2022-05-30 20:55:24 +0200

    Bullets fire from the middle of player sprite

--- src/scripts/weapons/Bullet.ts
+++ src/scripts/weapons/Bullet.ts
@@ -17,7 +17,12 @@ export default class Bullet {
     this.hitBox = new PIXI.Graphics();
     this.hitBox.beginFill(0xff0000);
     this.hitBox.x = 0;
-    this.hitBox.drawRect(Player.position.x, Player.position.y, 5, 5);
+    this.hitBox.drawRect(
+      Player.position.x + Player.sprite.width / 2,
+      Player.position.y + Player.sprite.height / 2,
+      5,
+      5
+    );
     this.hitBox.endFill();
     this.app.stage.addChild(this.hitBox);
   }
