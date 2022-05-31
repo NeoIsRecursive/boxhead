f22b86c3ea2142161edf68ac2c997e2e46a89f6e
DavisDavisDavis <olizoooom@gmail.com>
2022-05-31 13:13:31 +0200

    Checkout and rebase from master to weapons

--- src/scripts/weapons/pistol.ts
+++ src/scripts/weapons/pistol.ts
@@ -5,12 +5,26 @@ import Player from '../player/Player';
 export default class Pistol {
   constructor(Player: Player) {
     this.player = Player;
+    this.hitBox = new PIXI.Graphics();
   }
   player;
+  hitBox;
 
   fire() {
     if (this.player.firing()) {
-      console.log('BAM');
+      if (this.player.goingUp()) console.log('fire up');
+      if (this.player.goingDown()) console.log('fire down');
+      if (this.player.goingLeft()) console.log('fire left');
+      if (this.player.goingRight()) console.log('fire right');
     }
+  }
+
+  draw() {
+    // console.log(this.player.position);
+    this.hitBox.position.set(this.player.position.x, this.player.position.y);
+    this.hitBox
+      .lineStyle(20, 0xffffff)
+      .moveTo(0, 0)
+      .lineTo(this.player.vel.x, this.player.vel.y);
   }
 }
